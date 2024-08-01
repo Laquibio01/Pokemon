e365ce6bdc08ffd770476317cb64a1ede5f2b4c6
Farafolodolo <a1123150055@utch.edu.mx>
2024-07-31 23:35:20 -0700

    David Portillo - TestFinal
    Porque al Christian se le fue la onda

--- src/CallClasses.java
+++ src/CallClasses.java
@@ -23,12 +23,14 @@ public class CallClasses {
      
       
       datos.consultaUnPokemon("JuegoPokemon", "TPokemones", 1);
+      nombre1 = datos.nombre;
       Hp1 = datos.health;
       defensa1 = datos.defense;
       ataque1 = datos.atack;
       velocidad1 = datos.velocity;
       
       datos.consultaUnPokemon("JuegoPokemon", "TPokemones", 2);
+      nombre2 = datos.nombre;
       Hp2 = datos.health;
       defensa2 = datos.defense;
       ataque2 = datos.atack;
@@ -36,6 +38,8 @@ public class CallClasses {
       
       Pokemon pokemon1 = new Pokemon(nombre1, Hp1, ataque1, defensa1, velocidad1);
       Pokemon pokemon2 = new Pokemon(nombre2, Hp2, ataque2, defensa2, velocidad2);
+      
+      interfazMain.establecerVida(Hp1, Hp2);
       
       
       
